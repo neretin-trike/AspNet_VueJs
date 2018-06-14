0fb6bb5ba1f6d93f7edda7b922cdaf775ba11adc
Konstantin <konstantin.kastirin@actonica.ru>
2018-06-14 15:03:52 +0800

    feature (add-address): added a process for adding new address

--- AspNet_VueJs/App/AddressInput.vue
+++ AspNet_VueJs/App/AddressInput.vue
@@ -1,16 +1,16 @@
 <template>
 	<div class="modal-content">
-		<form action="/Address/Add" method="POST">
-			<label for="inputSurname">Фамилия</label>
-			<input type="text" name="inputSurname" id="inputSurname"/>
-			<label for="inputName">Имя</label>
-			<input type="text" name="inputName" id="inputName"/>
-			<label for="inputMiddlename">Отчество</label>
-			<input type="text" name="inputMiddlename" id="inputMiddlename"/>
-			<label for="inputAddress">Адрес</label>
-			<input type="text" name="inputAddress" id="inputAddress"/>
-			<label for="inputPhone">Номер телефона</label>
-			<input type="text" name="inputPhone" id="inputPhone"/>
+		<form v-on:submit.prevent v-on:submit="add">
+			<label for="surname">Фамилия</label>
+			<input type="text" name="surname" id="surname" v-model="surname"/>
+			<label for="name">Имя</label>
+			<input type="text" name="name" id="name" v-model="name"/>
+			<label for="middlename">Отчество</label>
+			<input type="text" name="middlename" id="middlename" v-model="middlename"/>
+			<label for="address">Адрес</label>
+			<input type="text" name="address" id="address" v-model="address"/>
+			<label for="phone">Номер телефона</label>
+			<input type="text" name="phone" id="phone" v-model="phone"/>
 			<input type="submit" value="Добавить"/>
 		</form>
 	</div>
@@ -18,7 +18,25 @@
 
 <script>
 	export default {
-		name: 'modal-content'
+		name: 'modal-content',
+		data: function() {
+			return {
+				surname: '',
+				name: '',
+				middlename: '',
+				address: '',
+				phone: ''
+			}
+		},
+		methods:{
+			add: function(event){
+				event.preventDefault();
+				var parameters = { surname : this.surname, name : this.name, middlename : this.middlename, address : this.address, phone : this.phone };
+				this.axios.post('/api/Address/Add', parameters).then(res => {
+					console.log(res)
+				});
+			}
+		}
 	}
 </script>
 
--- AspNet_VueJs/Controllers/AddressController.cs
+++ AspNet_VueJs/Controllers/AddressController.cs
@@ -25,5 +25,25 @@ namespace AspNet_VueJs.Controllers
             var result = AddressesViewModel.Instance.AddressesList.Where(a => a.Index == addressIndex).FirstOrDefault();
             return result;
         }
+
+        [HttpPost(nameof(Add))]
+        [Produces("application/json")]
+        public void Add([FromBody]AddressItemViewModel item)
+        {
+            int newIndex = 1;
+            while (true) {
+                var isIndexExists = AddressesViewModel.Instance.AddressesList.Any(address => address.Index == newIndex);
+
+                if (!isIndexExists)
+                {
+                    break;
+                }
+
+                newIndex++;
+            }
+
+            item.Index = newIndex;
+            AddressesViewModel.Instance.AddressesList.Add(item);
+        }
     }
 }
