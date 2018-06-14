7194268bdb8207146e2658163f29c6999ef0a865
Konstantin <konstantin.kastirin@actonica.ru>
2018-06-14 09:54:02 +0800

    refactor a config-files

--- AspNet_VueJs/wwwroot/dist/main.js
+++ AspNet_VueJs/wwwroot/dist/main.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "19146f699d6ae8a1a049"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "33f8814aa9444a298abd"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
