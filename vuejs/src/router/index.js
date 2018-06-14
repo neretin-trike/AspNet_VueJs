9421883894f08e6734e8d6dbbf60359f3dd1667d
Konstantin <konstantin.kastirin@actonica.ru>
2018-06-14 18:34:23 +0800

    refactor (vue): deleted a router for '\about'

--- vuejs/src/router/index.js
+++ vuejs/src/router/index.js
@@ -5,17 +5,11 @@ import addresses from '../components/Addresses.vue'
 
 Vue.use(VueRouter)
 
-const About = {template: '<h1>About</h1>'}
-
 let router = new VueRouter({
   routes:[
     {
       path: '/',
       component: addresses
-    },
-    {
-      path: '/about',
-      component: About
     }
   ]
 })
