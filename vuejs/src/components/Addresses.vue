8b2a802ae282070840e63a49348373e1430d4aad
Konstantin <konstantin.kastirin@actonica.ru>
2018-06-15 16:26:40 +0800

    refactor (vue): added a js-classes for address

--- vuejs/src/components/Addresses.vue
+++ vuejs/src/components/Addresses.vue
@@ -32,8 +32,7 @@
         data: function () {
             return {
                 addresses: [],
-                isModalVisible: false,
-                lastOrderNumber: 0
+                isModalVisible: false
             }
         },
         components:{
@@ -49,15 +48,32 @@
         },
         created() {
             this.axios.get('http://localhost:55464/api/Address/Addresses').then((response) => {
-                this.addresses = response.data;
-                for(var i=0; i<this.addresses.length; i++){
-                    this.addresses[i].addressUrl = '/Address/' + this.addresses[i].index;
-                    this.addresses[i].orderNumber = i + 1;
+                let addressesFromServer = response.data;
+                for(var i=0; i<addressesFromServer.length; i++){
+                    let addressUrl = '/Address/' + addressesFromServer[i].index;
+                    let orderNumber = i + 1;
+                    let addressView = new AddressView(addressesFromServer[i], addressUrl, orderNumber);
+                    this.addresses.push(addressView);
                 }
-                console.log(this.addresses);
             })
         }
     }
+
+    function Address(index, surname, name, middlename, address, phone){
+        this.index = index;
+        this.surname = surname;
+        this.name = name;
+        this.middlename = middlename;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    function AddressView(address, addressUrl, orderNumber){
+        Address.call(this, address.index, address.surname, address.name, address.middlename, address.address, address.phone);
+        this.addressUrl = addressUrl;
+        this.orderNumber = orderNumber;
+    }
+    AddressView.prototype = Object.create(Address.prototype)
 </script>
 
 <style scoped>
