5d915cfd7240bcf8a56045e4a21f591f1a567887
Konstantin <konstantin.kastirin@actonica.ru>
2018-06-15 14:15:55 +0800

    refactor (view): refactor a css-style to 'ModalDialogBase'

--- vuejs/src/components/modals/ModalDialogBase.vue
+++ vuejs/src/components/modals/ModalDialogBase.vue
@@ -1,16 +1,16 @@
 <template>
-    <transition name="modal-fade">
-        <div class="modal-backdrop">
+    <transition name="modal_fade">
+        <div class="modal_background">
             <div class="modal"
                  role="dialog"
                  aria-labelledby="modalTitle"
                  aria-describedby="modalDescription">
-                <header class="modal-header"
+                <header class="modal_header"
                         id="modalTitle">
                     <slot name="header">
                         <button
                             type="button"
-                            class="btn-close"
+                            class="btn_close"
                             v-on:click="close"
                         >
                             x
@@ -18,12 +18,12 @@
                     </slot>
                 </header>
 
-                <section class="modal-body"
+                <section class="modal_body"
                          id="modalDescription">
                     <slot name="body"></slot>
                 </section>
 
-                <footer class="modal-footer">
+                <footer class="modal_footer">
                     <slot name="footer"></slot>
                 </footer>
             </div>
@@ -43,65 +43,59 @@
 </script>
 
 <style scoped>
-    .modal-backdrop {
+    .modal_background {
+        display: flex;
+        justify-content: center;
+        align-items: center;
         position: fixed;
         top: 0;
         bottom: 0;
         left: 0;
         right: 0;
         background-color: rgba(0, 0, 0, 0.3);
-        display: flex;
-        justify-content: center;
-        align-items: center;
     }
 
     .modal {
+        display: flex;
+        flex-direction: column;
         background: #FFFFFF;
         box-shadow: 2px 2px 20px 1px;
         overflow-x: auto;
-        display: flex;
-        flex-direction: column;
     }
 
-    .modal-header,
-    .modal-footer {
-        padding: 15px;
-        display: flex;
-    }
-
-    .modal-header {
+    .modal_header {
+        text-align: right;
         border-bottom: 1px solid #eeeeee;
         color: #4AAE9B;
-        justify-content: space-between;
     }
 
-    .modal-footer {
-        border-top: 1px solid #eeeeee;
+    .modal_footer {
         justify-content: flex-end;
+        border-top: 1px solid #eeeeee;
     }
 
-    .modal-body {
+    .modal_body {
         position: relative;
-        padding: 20px 10px;
+        padding: 2.4rem 2rem;
     }
 
-    .btn-close {
+    .btn_close {
         border: none;
-        font-size: 20px;
-        padding: 20px;
+        font-size: 2.4rem;
+        padding: 1.4rem;
         cursor: pointer;
         font-weight: bold;
         color: #4AAE9B;
         background: transparent;
     }
 
-    .modal-fade-enter,
-    .modal-fade-leave-active {
+    .modal_fade-enter,
+    .modal_fade-leave-active {
         opacity: 0;
     }
 
-    .modal-fade-enter-active,
-    .modal-fade-leave-active {
+    .modal_fade-enter-active,
+    .modal_fade-leave-active {
         transition: opacity .5s ease;
     }
 </style>
